dd53be63e40a33260b03d5a89af4226deeb4feb6
bartosz.lagoda <composit095@gmail.com>
2024-02-25 13:05:31 +0100

    PKPP-0322: If else instruction

---
+++ 3.23_IF_ELSE_instruction.py
@@ -0,0 +1,11 @@
+print("Jestem pierwsza linia.")
+print("Jestem druga linia")
+glodny = True
+
+if glodny:
+    print("Musze cos zjesc! ")
+    print("Otwieram lodówkę...")
+else:
+    print("Jestem najedzony") # pojawi sie tylko w przypadku niespelnienia IF
+
+# wcięcia w pythonie wynoszą 4 spacje
