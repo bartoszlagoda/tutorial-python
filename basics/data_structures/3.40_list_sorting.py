daa527a97230dd55f6368dd7445560e87be9941b
bartosz.lagoda <composit095@gmail.com>
2024-02-25 20:57:05 +0100

    PKPP-0341: List sorting reversed

--- basics/data_structures/3.40_list_sorting.py
+++ basics/data_structures/3.40_list_sorting.py
@@ -10,3 +10,14 @@ print(krzysztof)
 print(leopold)
 print(robert)
 print(samuel)
+
+# jeśli nie chcemy zmieniać listy a tylko posortować ją w metodzie
+second_list = ["Krystyna", "Aleksandra", "Tamara", "Paulina", "Stanisława"]
+sorted_second_list = sorted(second_list)
+print(f"Bez sortowania \n{second_list}")
+print(f"Posortowana lista metodą sorted() : \n{sorted_second_list}")
+
+# jeśli chcemy posortować listę odwrotnie
+reverse_sorted_second_list = sorted(second_list,reverse=True)
+print(f"Posortowana lista metodą sorted() z argumentem reverse=True : \n{reverse_sorted_second_list}")
+
