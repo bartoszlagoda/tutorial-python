f02e89256f11a579b5ac946fa2dc8b959a157425
bartosz.lagoda <composit095@gmail.com>
2024-02-25 12:40:42 +0100

    PKPP-0318: Comparison Operathors

---
+++ 3.18_comparison_operathors.py
@@ -0,0 +1,18 @@
+# porównywanie różnych warunków i zwracanie TRUE albo FALSE
+
+# == równe
+print('Bartek' == 'Bartek')
+
+# != różne
+print('Bartek' != 'Bartosz')
+
+# znak wiekszosci > i znak mniejszosci <
+print(2 > 3)
+print ( 2 < 5)
+
+# >= czy większe lub równe
+print(2 >= 0)
+
+# <= czy mniejsze lub równe
+print(2 <= 0)
+
