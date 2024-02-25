7adb898f5b943a618aac28efb66ae1d094103463
bartosz.lagoda <composit095@gmail.com>
2024-02-25 17:55:04 +0100

    PKPP-0332: KWARGS

--- 3.32_KWARGS.py
+++ 3.32_KWARGS.py
@@ -1,4 +1,7 @@
 def rzeczy(*args):
+    print("=========================================")
+    print("                  RZECZY                 ")
+    print("=========================================")
     print("Wszystkie wpisane elementy to: ")
     for arg in args:
         print(arg)
@@ -6,8 +9,15 @@ def rzeczy(*args):
 
 # parametry nazwane kwargs oznaczają, że zawierają klucz i wartość do tego klucza
 def dziennik(klasa,**kwargs):
+    print("=========================================")
+    print("                 DZIENNIK                ")
+    print("=========================================")
     print(f'Klasa : {klasa}')
+    for nazwisko in kwargs.keys():
+        print(nazwisko)
+    print(kwargs.get('Kowalski'))
 
 
 
 rzeczy('lampa','koc','łóżko','telefon')
+dziennik('3c',Kowalski = 1, Nowak = 2, Wisniewski = 3)
