ebac154fe1f5fd2561442d9cb9b2a94ed4a9a9b0
bartosz.lagoda <composit095@gmail.com>
2024-02-25 12:03:56 +0100

    PKPP-0314: Math operators

---
+++ 3.14_math_operators.py
@@ -0,0 +1,23 @@
+# dodawanie +
+print(2+2)
+
+# odejmowanie -
+print(2-1)
+
+# mnozenie *
+print(2*5)
+
+# dzielenie /
+print(10/2)
+
+# potegowanie **
+print(5**10)
+
+# modulo %
+print(10%3) # reszta z dzielenia 10 przez 3 (3 jest 3 razy w 10, czyli reszta to 1)
+
+# dzielenie z zaokragleniem //
+print(12//7) # wynik to 1
+
+# mnozenie stringów
+print('Czesc ' * 10)
