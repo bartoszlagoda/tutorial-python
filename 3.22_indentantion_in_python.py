1e773f301df04a64f9c62f2d35b92139b4c1e537
bartosz.lagoda <composit095@gmail.com>
2024-02-25 13:02:55 +0100

    PKPP-0322: Indentantion in python

---
+++ 3.22_indentantion_in_python.py
@@ -0,0 +1,9 @@
+print("Jestem pierwsza linia.")
+print("Jestem druga linia")
+glodny = True
+
+if glodny:
+    print("Musze cos zjesc! ")
+    print("Otwieram lodówkę...")
+
+# wcięcia w pythonie wynoszą 4 spacje
