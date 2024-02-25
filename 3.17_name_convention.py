a7115918210fa11ceae297ed508e602c5c185f08
bartosz.lagoda <composit095@gmail.com>
2024-02-25 12:33:35 +0100

    PKPP-0317: Naming convention

---
+++ 3.17_name_convention.py
@@ -0,0 +1,24 @@
+# KONWENCJA NAZEWNICTWA W PYTHONIE
+pierwszaLiczba = 1
+_liczba = 2
+LICZBA = 3
+Liczba = 4
+liczba = 5
+liczbA = 6
+
+print(f"pierwszaLiczba = {pierwszaLiczba} \n"
+      f"_liczba = {_liczba} \n"
+      f"LICZBA = {LICZBA} \n"
+      f"Liczba = {Liczba} \n"
+      f"liczba = {liczba} \n"
+      f"liczbA = {liczbA}")
+
+# nazwy w pythonie są case sensitive, oznacza, to że wielkość liter ma znaczenie
+# w kursie będziemy korzystać z podkreślenie między wyrazami
+
+# złe nazwy zmiennych:
+# pierwsza-liczba
+# pierwsza liczba
+# 1_liczba
+# liczba$
+
