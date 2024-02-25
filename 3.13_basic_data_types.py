b360513c9076bc94d646f458e3e3198816a022d7
bartosz.lagoda <composit095@gmail.com>
2024-02-25 11:51:46 +0100

    PKPP-0313: Basic data types

---
+++ 3.13_basic_data_types.py
@@ -0,0 +1,20 @@
+# integers
+print(1)
+print(10323)
+
+# floating point numbers
+print(1.234)
+print(1232.912)
+
+print(12,3) # przecinek oznacza tylko przerwę między danymi
+
+# strings
+print("Hello, I am string")
+print('Hello I am string')
+
+# complex nubmers
+print(2+3j)
+
+# Boolean
+print(True)
+print(False)
