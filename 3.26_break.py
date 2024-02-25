6afbaa39192fc265145cfd0d0cec39a4c93c717f
bartosz.lagoda <composit095@gmail.com>
2024-02-25 13:49:11 +0100

    PKPP-0326: Key word break

--- 3.26_break.py
+++ 3.26_break.py
@@ -2,4 +2,6 @@ index = 0
 
 while index < 10:       # wykonuj instrukcję aż indeks dojdzie do 9
     print(index)        # drukuj na ekranie index
+    if index == 2:      # jeśli indeks będzie równy 2
+        break           # przerwij pętlę za pomocą 'break'
     index = index + 1   # zwiększaj index o 1
