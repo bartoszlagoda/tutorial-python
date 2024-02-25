e8197cb9e26cb464b70d89252c1ec36daa6c95c0
bartosz.lagoda <composit095@gmail.com>
2024-02-25 20:49:23 +0100

    PKPP-0340: List sorting

---
+++ basics/data_structures/3.40_list_sorting.py
@@ -0,0 +1,12 @@
+first_list = ["Krzysztof","Andrzej","Leopold","Samuel","Robert"]
+# sortowanie listy alfabetycznie
+first_list.sort()
+print(first_list)
+
+# przypisanie elementów z listy do zmiennych
+andrzej,krzysztof,leopold,robert,samuel = first_list
+print(andrzej)
+print(krzysztof)
+print(leopold)
+print(robert)
+print(samuel)
