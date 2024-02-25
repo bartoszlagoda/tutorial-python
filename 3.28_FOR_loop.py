c6b429c5d896e4c85d649d04aad15ddd1c40debf
bartosz.lagoda <composit095@gmail.com>
2024-02-25 14:20:21 +0100

    PKPP-0328: For loop

---
+++ 3.28_FOR_loop.py
@@ -0,0 +1,30 @@
+print("Pętla for od domyślnego 0 do 10: ")
+print("=================================")
+
+# for [zmienna] w zakresie [zakres]
+for number in range(10): # do 10 od domyślnego 0
+    print(number)
+
+print("Pętla for od 0 do 10: ")
+print("=================================")
+
+for number in range(0,10): # od 0 do 10
+    print(number)
+
+print("Pętla for od 2 do 10: ")
+print("=================================")
+
+for number in range(2,10): # od 2 do 10
+    print(number)
+
+print("Pętla for od 2 do 10 co 2: ")
+print("=================================")
+
+for number in range(2,10,2): # od 2 do 10 co 2
+    print(number)
+
+# pętla for w wyrazach
+print("Iteruj po wszystkich literach w słowie 'Bartosz': ")
+
+for character in 'Bartosz':
+    print(character)
