0876bc684523522adde65bd2e3bf44023710c618
bartosz.lagoda <composit095@gmail.com>
2024-02-25 12:22:23 +0100

    PKPP-0316: Variables

---
+++ 3.16_Variables.py
@@ -0,0 +1,7 @@
+first_number = 2
+second_number = 3
+sum_of_numbers = first_number + second_number
+
+name = input("Wprowadz swoje imie: ")                                     # wprowadzanie danych przez użytkownika
+print('Moje imie to ' + name)                                             # wydrukowanie na ekranie danych typu string
+print(f'Suma liczb {first_number} i {second_number} to {sum_of_numbers}') # przez formatowanie tworzymy szablon i wstawiamy w nim poszczególne dane
