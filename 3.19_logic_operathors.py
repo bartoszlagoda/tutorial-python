7ad16dba2e94572b12ea5cd748e29b04dcb3b949
bartosz.lagoda <composit095@gmail.com>
2024-02-25 12:46:32 +0100

    PKPP-0319: Logic Operathors

---
+++ 3.19_logic_operathors.py
@@ -0,0 +1,18 @@
+# AND (obydwa warunki muszą dać TRUE)
+print("Operator AND")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+# OR (jeden z warunków musi dać TRUE)
+print("Operator FOR")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+
+# NOT (zaprzeczenie)
+print('Operator NOT')
+print(not True)
+print(not False)
